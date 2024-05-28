92af76b557a281c3365c70c6db3bbfa091dfc1c3
ishaan-07x <iishaansingh@yahoo.com>
2024-05-28 17:43:32 +0000

    changed

---
+++ 01_baics/03_conversationOpreations.js
@@ -0,0 +1,40 @@
+// let score = null
+// let number = "07abc"
+// let boolean = true;
+
+// console.log(score);
+
+// console.log(typeof null);
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let numberConverted = Number(number)
+// console.log(numberConverted);
+// console.log(typeof number)
+// console.log(typeof (number));
+
+// let booleanConverted = Number(boolean)
+// console.log(booleanConverted)
+// console.log(typeof boolean)
+// console.log(typeof booleanConverted)
+
+
+//"07" => 07
+//"07abc" => NaN
+// true => 1; false => 0;
+
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false 
+// "" => false
+//"Ishaan" => true
+let someNumber = 7
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
