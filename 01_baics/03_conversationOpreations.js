3e23ac2f649cfc741dcfba397eca0d3c6381d81b
ishaan-07x <iishaansingh@yahoo.com>
2024-05-29 06:58:07 +0000

    types added

--- 01_baics/03_conversationOpreations.js
+++ 01_baics/03_conversationOpreations.js
@@ -34,7 +34,28 @@
 // 1 => true; 0=>false 
 // "" => false
 //"Ishaan" => true
-let someNumber = 7
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// let someNumber = 7
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// console.log("7" + 3);
+// console.log(7 + "3");
+// console.log("7"+ 3 + 3);
+// console.log(3+3 + "7");
+
+// console.log((5+5)*4 %2);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1= num2=num3 = 2 + 2;
+
+let x = 3;
+let y = x++;
+console.log(y);
+let w = 9;
+let z = ++w;
+console.log(z);
---
+++ 01_baics/dataTypes-summary.js
@@ -0,0 +1,32 @@
+//Primmitve DataTypes
+
+// 7 types: String, Number, Boolean, null, undefined
+// Symbol, BigInt
+
+// const score  = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null
+// let userEmail;
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+// console.log(id=== anotherId)
+
+// RefrenceType or NonPrimmitvie
+
+
+//Array, Objects, Functions
+
+const heros = ["shakitman", "naagraj", "doga"];
+let myObj = {
+    name:"Ishaan",
+    age:2
+};
+
+const myFunction = function(){
+    console.log("Hello World")
+
+}
+// Function ka return type objFunction hota hai
+//NonPrimmitve ka Object he aata hai 
+console.log(typeof heros)
