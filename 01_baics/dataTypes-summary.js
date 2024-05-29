33fdec9fcd8ce69bd070b1b7df0a5b122833b618
ishaan-07x <iishaansingh@yahoo.com>
2024-05-29 07:15:56 +0000

    new code

--- 01_baics/dataTypes-summary.js
+++ 01_baics/dataTypes-summary.js
@@ -12,21 +12,52 @@
 // const anotherId = Symbol('123');
 // console.log(id=== anotherId)
 
-// RefrenceType or NonPrimmitvie
+// // RefrenceType or NonPrimmitvie
 
 
-//Array, Objects, Functions
+// //Array, Objects, Functions
 
-const heros = ["shakitman", "naagraj", "doga"];
-let myObj = {
-    name:"Ishaan",
-    age:2
-};
+// const heros = ["shakitman", "naagraj", "doga"];
+// let myObj = {
+//     name:"Ishaan",
+//     age:2
+// };
 
-const myFunction = function(){
-    console.log("Hello World")
+// const myFunction = function(){
+//     console.log("Hello World")
 
-}
-// Function ka return type objFunction hota hai
-//NonPrimmitve ka Object he aata hai 
-console.log(typeof heros)
+// }
+// // Function ka return type objFunction hota hai
+// //NonPrimmitve ka Object he aata hai 
+// console.log(typeof heros)
+
+
+
+//Stack(primmitve) and Heap(NonPrimmitve)
+
+//Stack ---> gives copy of original value
+
+// let myNumber = 3;
+// let myNumberTwo = myNumber;
+// myNumberTwo = 5;
+// console.log(myNumber);
+// console.log(myNumberTwo);
+
+
+//Heap --> gives refrence of Original vale
+
+
+// let userOne={
+//     email:"user@google.com",
+//     upi:"user@paytm"
+// }
+// let userTwo = userOne;
+// userTwo.email = "ishaan@gmail.com"
+
+// console.log(userOne.email, userTwo.email);
+
+
+
+// let y = 3;
+// let x = y++;
+// console.log(`value of x is :${x}, value of y is:${y}`);
