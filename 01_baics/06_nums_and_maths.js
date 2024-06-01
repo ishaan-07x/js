5ecd3deb0a55fb5947e6ded8a74a2f7568b694c3
ishaan-07x <iishaansingh@yahoo.com>
2024-06-01 05:58:24 +0000

    dates

--- 01_baics/06_nums_and_maths.js
+++ 01_baics/06_nums_and_maths.js
@@ -34,9 +34,9 @@
 
 
 //console.log(Math.random())
-console.log(Math.floor(Math.random()*10)+1)
-console.log(Math.floor(Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
 
-const min = 10;
-const max = 20;
-console.log(Math.floor(Math.random()* (max- min +1))+min)
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random()* (max- min +1))+min)
---
+++ 01_baics/07_datesInJs.js
@@ -0,0 +1,19 @@
+let myDate = new Date()
+// console.log(myDate.toDateString())
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleDateString())
+
+
+
+// let myCreatedDate = new Date(2024,5,1)
+// console.log(myCreatedDate.toDateString())
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(Math.floor(Date.now()/1000))
+
+
+
+
+// let newDate = new Date()
+// console.log(newDate.getMonth())
