991b8325b6209625d459740dfc526d4e0562ac52
ishaan-07x <iishaansingh@yahoo.com>
2024-06-01 05:38:24 +0000

    maths and numbers

---
+++ 01_baics/06_nums_and_maths.js
@@ -0,0 +1,42 @@
+// const score = 400;
+// console.log(score)
+
+// const balance = new Number(100);
+// console.log(balance)
+// const bal = Number(500)
+// console.log(bal)
+
+
+
+// console.log(typeof score, typeof balance, typeof bal)
+// console.log(balance.toFixed())
+// console.log(score.toString().length)
+
+// const otherNumber = 23.77
+
+// console.log(otherNumber.toPrecision(3));
+
+
+// const hundreds = 100000
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.max(2,4,5,6,6,7,8))
+// console.log(Math.min(2,3,67))
+
+
+
+//console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()* (max- min +1))+min)
